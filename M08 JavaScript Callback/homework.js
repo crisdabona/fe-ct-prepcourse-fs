2158ccbcc7e4014862fffd3f89a94944013a5590
crisdabona <cdvillabonabr@gmail.com>
2023-08-17 22:48:33 -0300

    Ejer8 complete

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,17 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   let arr = nombre.split('')
+   arr[0] = arr[0].toUpperCase()
+   let capitalize = arr.join('')
+   return capitalize
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb()
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +23,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1, num2)
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +32,8 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   let suma = arrayOfNumbers.reduce((a,b) => a + b)
+   cb(suma)
 }
 
 function forEach(array, cb) {
@@ -33,6 +41,7 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach(el => cb(el))
 }
 
 function map(array, cb) {
@@ -40,13 +49,28 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   let arr = []
+   array.forEach(el => arr.push(cb(el)))
+   return arr
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let newArr = []
+   
+   arrayOfStrings.map(el => {
+      if(el[0] === 'a'){
+         newArr.push(el)
+      }
+   })
+   
+   return newArr
 }
+
+let nombre = mayuscula('cristian')
+console.log(nombre)
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
